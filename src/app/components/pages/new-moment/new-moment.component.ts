07bee2af65df091ec437d0361d50402ebf578fef
jose alisson bezerra <josealissonbezerra@hotmail.com>
2024-02-24 22:13:47 -0300

    feat: starting project

---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.scss']
+})
+export class NewMomentComponent implements OnInit {
+  btnText = 'Compartilhar';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
